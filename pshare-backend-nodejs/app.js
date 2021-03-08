e5143dd0b71e648027afca47fd9c275a1fa3a6f9
jxu477 <jxu477@gatech.edu>
2021-03-08 11:48:47 -0500

    allow headers modify

--- pshare-backend-nodejs/app.js
+++ pshare-backend-nodejs/app.js
@@ -12,8 +12,8 @@ const app = express();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept');
     //intercepts OPTIONS method
     if ('OPTIONS' === req.method) {
         //respond with 200
